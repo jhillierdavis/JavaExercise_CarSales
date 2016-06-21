eb22a46bc9e569937bd0b6552d0c150eea3635e3
Joe Hillier-Davis <joe.hillier-davis@tribalworldwide.co.uk>
2016-06-21 13:55:44 +0100

    Amended to use Java 8 Streams, plus added additional unit tests for exceptional cases.

--- src/main/java/sales/services/CarModelFilter.java
+++ src/main/java/sales/services/CarModelFilter.java
@@ -3,6 +3,7 @@ package sales.services;
 import sales.model.SalesDatum;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 /**
  * Filters car model info. from sales data.
@@ -23,49 +24,6 @@ class CarModelFilter {
     }
 
     protected void processData(final SalesDatum[] data)  {
-        String[] allCarModelEntries = getAllCarModelEntries(data);
-        int uniqueCount = getUniqueCarModelCount(allCarModelEntries);
-        this.uniqueCarModels = getUniqueCarModels(allCarModelEntries, uniqueCount);
+        this.uniqueCarModels = Arrays.stream(data).map(d -> d.getCarModel()).distinct().toArray(String[]::new);
     }
-
-
-    private String[] getAllCarModelEntries(final SalesDatum[] data) {
-        int length = data.length;
-        String[] allCarModelEntries = new String[length];
-
-        for (int i =0; i < length; i++ ) {
-            allCarModelEntries[i] = data[i].getCarModel();
-        }
-        return allCarModelEntries;
-    }
-
-    private int getUniqueCarModelCount(String[] allCarModelEntries) {
-        int uniqueCount = 0;
-
-        Arrays.sort(allCarModelEntries);
-        String lastCarModel = "";
-        for (String currentCarModel: allCarModelEntries)  {
-            if (!currentCarModel.equals(lastCarModel))    {
-                uniqueCount++;
-                lastCarModel = currentCarModel;
-            }
-        }
-        return uniqueCount;
-    }
-
-    private String[] getUniqueCarModels(String[] allCarModelEntries, final int uniqueCount) {
-        String[] uniqueCarModels = new String[uniqueCount];
-        String lastCarModel = "";
-
-        int offset = uniqueCount;
-        for (String carModel: allCarModelEntries)  {
-            if (!carModel.equals(lastCarModel))    {
-                uniqueCarModels[uniqueCarModels.length - offset] = carModel;
-                offset--;
-                lastCarModel = carModel;
-            }
-        }
-        return uniqueCarModels;
-    }
-
 }
--- src/test/java/sales/services/CarModelFilterTest.java
+++ src/test/java/sales/services/CarModelFilterTest.java
@@ -2,9 +2,17 @@ package sales.services;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 import sales.model.SalesDatum;
 
+
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+
 
 /**
  * Unit tests for CarModelFilter class.
@@ -19,17 +27,36 @@ public class CarModelFilterTest {
     }
 
     @Test
-    public void getUniqueCarModels()  {
-        // Given:
+    public void getUniqueCarModels() {
+        // Given: A set of test data
         CarModelFilter filter = new CarModelFilter(testData);
 
         // When: Unique models are filtered
         String[] modelArray = filter.getUniqueCarModels();
         int length = modelArray.length;
 
-        // Then: The expected number of car models are identified
+        // Then: The expected unique car models are identified
         assertTrue("Unexpected length=" + length, length == 3);
+        assertThat( Arrays.asList(modelArray), hasItem("Golf") );
+        assertThat( Arrays.asList(modelArray), hasItem("Polo") );
+        assertThat( Arrays.asList(modelArray), hasItem("Scirocco") );
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void handleNullData()    {
+         new CarModelFilter(null);
+    }
+
+    @Test
+    public void handleEmptyData()    {
+        // Given: An empty data set
+        CarModelFilter filter = new CarModelFilter(new SalesDatum[0]);
+
+        // When: Filtered
+        String[] modelArray = filter.getUniqueCarModels();
+
+        // Then: An empty array is returned
+        assertThat(modelArray.length, is(0));
+    }
 
 }
