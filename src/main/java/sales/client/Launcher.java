dff842139575e3945dfbffcb4236b8b1584304ce
Joe Hillier-Davis <joe.hillier-davis@tribalworldwide.co.uk>
2016-06-28 17:40:24 +0100

    Added execution entry point for launching an InteractiveConsole instance.

---
+++ src/main/java/sales/client/Launcher.java
@@ -0,0 +1,12 @@
+package sales.client;
+
+/**
+ * Execution entry point for launching an InteractiveConsole instance.
+ */
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        new InteractiveConsole();
+    }
+}
